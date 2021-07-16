cb80b1f09ad10f6ca1ae51f74a055d9719bd07e5
Fabian Pacherres <fabianpacherres@gmail.com>
2021-07-16 00:31:46 -0500

    Header creation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="bg-red-600">
+    <px-header />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PxHeader from "@/components/global/PxHeader"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {PxHeader}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
