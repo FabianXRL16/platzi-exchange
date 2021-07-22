71b4118332a9d59e8911796e6d09567394d08a1c
Fabian Pacherres <fabianpacherres@gmail.com>
2021-07-21 19:22:20 -0500

    Button home

--- src/components/global/PxHeader.vue
+++ src/components/global/PxHeader.vue
@@ -4,9 +4,8 @@
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <px-icon />
-          <span class="font-semibold text-xl tracking-tight ml-4"
-            >PlatziExchange</span
-          >
+          <router-link to="/" class="font-semibold text-xl tracking-tight ml-4"
+            >PlatziExchange</router-link>
         </div>
         <div
           class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto"
