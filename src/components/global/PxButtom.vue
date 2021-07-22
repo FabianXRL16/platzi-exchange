06ec4ce3e85378f13143c391657bf3ffe9971d50
Fabian Pacherres <fabianpacherres@gmail.com>
2021-07-21 19:08:00 -0500

    created buttom

---
+++ src/components/global/PxButtom.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    @click="onClick()"
+    class="border border-yellow-500 text-yellow-500 font-medium px-4 py-2 rounded-full"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PxButtom",
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
